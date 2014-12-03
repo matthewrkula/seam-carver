48a31324861e1cbffc31d0f1a71e19b1d7041659
Matt Kula <matthewrkula@gmail.com>
2014-12-02 22:57:54 -0600

    Commit main

--- src/com/mattkula/seamcarver/Main.java
+++ src/com/mattkula/seamcarver/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //            new Carver("/home/matt/Pictures/core05-4.png").carve();
 //            new Carver("/home/matt/Pictures/castle.png").carve();
 //            for (int i = 0; i < 40; i++) {
-                new Carver("/home/matt/Pictures/lake.png").carve();
+                new Carver("/home/matt/Pictures/castle.png").carve();
 //            }
 
             System.out.println(String.format("%dms", System.currentTimeMillis() - time));
